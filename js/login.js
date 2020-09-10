dc72f3ac23ec01ef7461fad462b65357382aabb3
Federico2198 <federicoguillermomoreiratejera@gmail.com>
2020-09-10 18:05:21 -0300

    Segunda entrega 30/08/2020 sin desafiate

--- js/login.js
+++ js/login.js
@@ -1,12 +1,19 @@
 function login(){
     let usuario = document.getElementById("user").value;
     let contraseña = document.getElementById("pass").value;
+    let localusuario;
+
     if (usuario.trim()==="" || contraseña.trim()===""){
         alert("Por favor ingrese un usuario o contraseña correcta");   
     }
     else{
         window.location="index.html";
+        localStorage.setItem("usuario",usuario)
     }
 };
     document.addEventListener("DOMContentLoaded", function(e){
-});
+})
+function logout(){
+    localStorage.clear();
+    window.location="login.html";
+    }
